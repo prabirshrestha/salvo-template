00f66e70d39969b5a4abc63d5d73338f90ae0463
Prabir Shrestha <mail@prabir.me>
2024-03-29 20:05:59 -0700

    add styles (#11)

---
+++ crates/server/src/routes/assets.rs
@@ -0,0 +1,18 @@
+use salvo::prelude::*;
+
+const STYLES_CSS: &str = grass::include!("./crates/server/assets/stylesheets/styles.scss");
+
+pub fn router() -> Router {
+    Router::new().path("assets/styles.css").get(get_stylesheet)
+}
+
+#[handler]
+fn get_stylesheet(res: &mut Response) {
+    // TODO: handle release builds with hashed filenames and cache
+    res.render(Text::Css(STYLES_CSS))
+}
+
+pub fn styles_css_href<'a>() -> &'a str {
+    // TODO: handle release builds with hashed filenames and cache
+    "/assets/styles.css"
+}
--- crates/server/src/views/layout.rs
+++ crates/server/src/views/layout.rs
@@ -1,9 +1,12 @@
+use crate::routes::assets;
+
 markup::define! {
     Layout<Main: markup::Render>(main: Main) {
         @markup::doctype()
         html {
             head {
                 title { "App" }
+                link[rel="stylesheet", href=assets::styles_css_href()] {}
             }
             body {
                 main {
