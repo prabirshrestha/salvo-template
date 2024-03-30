c04fccb2823f29cd7ba19650bbfea49fa62a569a
Prabir Shrestha <mail@prabir.me>
2024-03-29 20:00:58 -0700

    add styles

--- crates/server/src/routes/mod.rs
+++ crates/server/src/routes/mod.rs
@@ -1,6 +1,7 @@
 pub use salvo::prelude::*;
+pub mod assets;
 mod home;
 
 pub fn router() -> Router {
-    Router::new().push(home::routes())
+    Router::new().push(home::routes()).push(assets::router())
 }
