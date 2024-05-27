611184f192630d98694b8c241c4ae5815fd41d33
Prabir Shrestha <prshrest@microsoft.com>
2024-05-27 14:04:29 -0700

    move stylesheets to assets folder

--- crates/server/src/build.rs
+++ crates/server/src/build.rs
@@ -4,7 +4,7 @@ fn main() -> Result<()> {
     let mut ructe = Ructe::from_env()?;
     let mut statics = ructe.statics()?;
     statics.add_files("assets")?;
-    statics.add_sass_file("stylesheets/style.scss")?;
+    statics.add_sass_file("assets/stylesheets/style.scss")?;
     ructe.compile_templates("templates")?;
     Ok(())
 }
