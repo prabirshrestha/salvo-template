ac9cf76e75150d8c5b8cb29b07e3b7b6f8d21106
Prabir Shrestha <mail@prabir.me>
2024-08-25 20:48:27 -0700

    add cli args parser

--- crates/server/src/build.rs
+++ crates/server/src/build.rs
@@ -1,10 +1,27 @@
-use ructe::{Result, Ructe};
+use ructe::Ructe;
+use vergen::Emitter;
+use vergen_gitcl::GitclBuilder;
 
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
+    let gitcl = GitclBuilder::all_git()?;
+
+    if let Err(error) = Emitter::default()
+        .add_instructions(&gitcl)?
+        .fail_on_error()
+        .emit()
+    {
+        println!(
+            "cargo:warning=Could not generate git version information: {:?}",
+            error
+        );
+        println!("cargo:rustc-env=VERGEN_GIT_SHA=nogit");
+    }
+
     let mut ructe = Ructe::from_env()?;
     let mut statics = ructe.statics()?;
     statics.add_files("assets")?;
     statics.add_sass_file("assets/stylesheets/style.scss")?;
     ructe.compile_templates("templates")?;
+
     Ok(())
 }
